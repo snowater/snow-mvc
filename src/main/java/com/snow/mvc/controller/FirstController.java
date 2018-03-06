8d55bee00321482fcf23da217a8e7699ada775ef
snow <wanghuiqi2012@163.com>
2018-03-06 15:04:20 +0800

    add service and controller

---
+++ src/main/java/com/snow/mvc/controller/FirstController.java
@@ -0,0 +1,19 @@
+/**
+ * @(#) FirstController.java 2018/03/06
+ * Copyright 2018 Snow.com, Inc. All rights reserved.
+ */
+package com.snow.mvc.controller;
+
+import com.snow.mvc.annotation.SnowController;
+import com.snow.mvc.annotation.SnowRequestMapping;
+
+/**
+ * @author snow
+ * @version 2018/03/06
+ */
+@SnowController
+@SnowRequestMapping(value = "/web")
+public class FirstController {
+
+
+}
