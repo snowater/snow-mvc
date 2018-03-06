776718a7ee9c236157a92917510c204e79b0e1f6
hzwanghuiqi <hzwanghuiqi@corp.netease.com>
2018-03-06 14:48:28 +0800

    初始化项目结构

---
+++ src/main/java/com/snow/mvc/annotation/SnowAutowired.java
@@ -0,0 +1,12 @@
+/**
+ * @(#) SnowAutowired.java 2018/03/06
+ * Copyright 2018 Snow.com, Inc. All rights reserved.
+ */
+package com.snow.mvc.annotation;
+
+/**
+ * @author snow
+ * @version 2018/03/06
+ */
+public class SnowAutowired {
+}
---
+++ src/main/java/com/snow/mvc/annotation/SnowController.java
@@ -0,0 +1,12 @@
+/**
+ * @(#) SnowController.java 2018/03/06
+ * Copyright 2018 Snow.com, Inc. All rights reserved.
+ */
+package com.snow.mvc.annotation;
+
+/**
+ * @author snow
+ * @version 2018/03/06
+ */
+public class SnowController {
+}
